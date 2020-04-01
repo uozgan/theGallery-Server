dd39e79730354ba48ac662b612963d3942bc3c11
uozgan <utkuozgan_8@hotmail.com>
2020-04-01 21:29:51 +0200

    Bug catching

--- routers/artworks.js
+++ routers/artworks.js
@@ -5,39 +5,6 @@ const Bid = require("../models").bid;
 
 const router = new Router();
 
-router.get("/", async (req, res) => {
-  const limit = req.query.limit || 10;
-  const offset = req.query.offset || 0;
-  const artworks = await Artwork.findAndCountAll({
-    limit,
-    offset,
-    include: [Bid]
-  });
-  console.log("artworks", artworks);
-
-  res.status(200).send({ message: "ok", artworks });
-});
-
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-
-  console.log(id);
-  if (isNaN(parseInt(id))) {
-    return res.status(400).send({ message: "Artwork id is not a number" });
-  }
-
-  const artwork = await Artwork.findByPk(id, {
-    include: [Bid],
-    order: [[Bid, "createdAt", "DESC"]]
-  });
-
-  if (artwork === null) {
-    return res.status(404).send({ message: "Artwork not found" });
-  }
-
-  res.status(200).send({ message: "ok", artwork });
-});
-
 router.patch("/:id", async (req, res) => {
   const artwork = await Artwork.findByPk(req.params.id);
 
@@ -105,6 +72,39 @@ router.post("/auction", auth, async (req, res) => {
   return res
     .status(201)
     .send({ message: "Artwork successfully created", artwork });
+});
+
+router.get("/", async (req, res) => {
+  const limit = req.query.limit || 10;
+  const offset = req.query.offset || 0;
+  const artworks = await Artwork.findAndCountAll({
+    limit,
+    offset,
+    include: [Bid]
+  });
+  console.log("artworks", artworks);
+
+  res.status(200).send({ message: "ok", artworks });
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+  if (isNaN(parseInt(id))) {
+    return res.status(400).send({ message: "Artwork id is not a number" });
+  }
+
+  const artwork = await Artwork.findByPk(id, {
+    include: [Bid],
+    order: [[Bid, "createdAt", "DESC"]]
+  });
+
+  if (artwork === null) {
+    return res.status(404).send({ message: "Artwork not found" });
+  }
+
+  res.status(200).send({ message: "ok", artwork });
 });
 
 module.exports = router;
